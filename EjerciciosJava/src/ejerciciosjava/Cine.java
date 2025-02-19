8d25156f701fa815328ed02db889061093a5e494
HeinnerGN <heinnergil@gmail.com>
2025-02-19 16:13:20 -0500

    .

--- EjerciciosJava/src/ejerciciosjava/Cine.java
+++ EjerciciosJava/src/ejerciciosjava/Cine.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejerciciosjava;
 
-/**
- *
- * @author heinn
- */
+import java.util.Scanner;
+
 public class Cine {
+    
+    public Cine(){
+        System.out.println("Ejercicio 8: Cine");
+        System.out.println("");
+    }
+    
+    public static void tomardatos(){
+        Scanner dato = new Scanner(System.in);
+        System.out.println("Ingrese su edad:");
+        int edad=dato.nextInt();
+    }
     
 }
