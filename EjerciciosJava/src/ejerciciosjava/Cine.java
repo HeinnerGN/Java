5104e92d67bb92b4e0ad2482b61eb7d7ceb92906
HeinnerGN <heinnergil@gmail.com>
2025-02-21 12:53:54 -0500

    .

--- EjerciciosJava/src/ejerciciosjava/Cine.java
+++ EjerciciosJava/src/ejerciciosjava/Cine.java
@@ -18,9 +18,42 @@ public class Cine {
     }
     
     public static void procesardatos(int edad){
+        String resultado=null;
         if(edad<7){
-            resultado=""
+            resultado="Niños";
         }
+        else if(edad>=7 && edad<=17){
+            resultado="Niños y Adolescentes";
+        }
+        else if(edad>17 && edad<31){
+            resultado="Adultos";
+        }
+        else if(edad>30){
+            resultado="Personas Mayores";
+        }
+        
+        switch(resultado){
+            case "Niños":
+                System.out.println("Se recomienda peliculas animadas y educativas aptas para todas las edades.");
+                break;
+            case "Niños y Adoslecentes":
+                System.out.println("Se recomienda peliculas animadas, aventuras y comedias familiares.");
+                break;
+            case "Adultos":
+                System.out.println("Se recomienda una variedad de peliculas de generos, accion, drama, comedia y ciencia ficcion ");
+                break;
+            case "Personas Mayores":
+                System.out.println("Se recomienda peliculas clasicas y dramas que puede ser de su interes.");
+                break;
+            default:
+                System.out.println("Datos invalidos.");
+                break;
+        }
+        mostrardatos(resultado);
+    }
+    
+    public static void mostrardatos(String resultado){
+        System.out.println("Disfrute de sus peliculas.");
     }
     
 }
